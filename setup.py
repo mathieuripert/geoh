abb9d4f241d5faa1d349ca8c332cc09990cbb33c
John Miller <john.miller@billups.com>
2020-08-14 17:17:23 -0700

    Update to version 0.4

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ from setuptools import find_packages
 
 required = [
     "pandas>=0.15.2",
-    "geopandas>=0.2",
     "shapely>=1.5.13",
     "python-geohash>=0.8.5"
 ]
 
 setup(
     name="geoh",
-    version="0.3",
+    version="0.4",
     author="Mathieu Ripert",
-    author_email="mathieu@instacart.com",
-    url="https://github.com/mathieuripert/geoh",
+    author_email="john.miller@billups.com",
+    url="https://github.com/billups/geoh",
     license="MIT",
     packages=find_packages(),
     package_dir={"geoh": "geoh"},
